cd488d6a49cbab0332635059e2f539d9c6400102
Ine Picone <inespicone@hotmail.com>
2022-05-25 15:22:50 -0300

    allow files and get hash ready

--- api/utils.py
+++ api/utils.py
@@ -1,10 +1,14 @@
+from   curses             import flash
+from   functools          import partial
+from nis import cat
 import os
-
+import hashlib
+from   tkinter.messagebox import WARNING
 
 def allowed_file(filename):
     """
     Checks if the format for the file received is acceptable. For this
-    particular case, we must accept only image files.
+    particular case, we must accept only image files. ['png', 'jpg', 'jpeg', 'gif']
 
     Parameters
     ----------
@@ -18,8 +22,17 @@ def allowed_file(filename):
     """
     # Current implementation will allow any kind of file.
     # TODO
-    return True
 
+    # formato de archivos permitidos:
+    # usamos tuplas porque es mas rapido
+    ALLOWED_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif"}
+
+    # _ variable de descarte
+    ext = os.path.splitext(filename)[1].lower()
+
+    # instaed of using if + else --> valid_file = ext in ALLOWED_EXTENSIONS esto me retorna true si esta en la lista que le pase o false si no esta
+    valid_file = ext in ALLOWED_EXTENSIONS
+    return valid_file
 
 def get_file_hash(file):
     """
@@ -37,6 +50,16 @@ def get_file_hash(file):
     str
         New filename based in md5 file hash.
     """
+    
     # Current implementation will return the original file name.
     # TODO
-    return os.path.basename(file.filename)
+    md5 = hashlib.md5(file.read()).hexdigest()
+    
+    ext = os.path.splitext(file.filename)[1].lower()
+
+    # return --> md5.hexdigest() + ext  
+    ret = f'{md5}{ext}'
+    
+    file.seek(0)
+    
+    return ret
