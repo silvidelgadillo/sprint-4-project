b310d8e910878ceff7006727308d69fd011516f2
NMengo <nico.mengoo@gmail.com>
2022-05-26 15:41:38 -0300

    finished "baseline" prediction
    finished classify_process

--- model/ml_service.py
+++ model/ml_service.py
@@ -13,9 +13,6 @@ db = redis.Redis(
     db=settings.REDIS_DB_ID
 )
 
-queue_name, msg = db.brpop(settings.REDIS_QUEUE)
-msg = json.loads(msg)
-image_name = msg['image_name']
 
 def predict(image_name):
     """
@@ -36,44 +33,23 @@ def predict(image_name):
     # TODO
     return 'Cat', 0.9999
 
-pred_class, pred_score = predict(image_name)
-
-output_msg = {
-    'prediction': pred_class,
-    'score': pred_score
-}
-
-db.set(msg['id'], json.dumps(output_msg))
 
 def classify_process():
-    """
-    Loop indefinitely asking Redis for new jobs.
-    When a new job arrives, takes it from the Redis queue, uses the loaded ML
-    model to get predictions and stores the results back in Redis using
-    the original job ID so other services can see it was processed and access
-    the results.
-
-    Load image from the corresponding folder based on the image name
-    received, then, run our ML model to get predictions.
-    """
     while True:
-        # Inside this loop you should add the code to:
-        #   1. Take a new job from Redis
-        #   2. Run your ML model on the given data
-        #   3. Store model prediction in a dict with the following shape:
-        #      {
-        #         "prediction": str,
-        #         "score": float,
-        #      }
-        #   4. Store the results on Redis using the original job ID as the key
-        #      so the API can match the results it gets to the original job
-        #      sent
-        # Hint: You should be able to successfully implement the communication
-        #       code with Redis making use of functions `brpop()` and `set()`.
-        # TODO
-
-        # Don't forget to sleep for a bit at the end
         time.sleep(settings.SERVER_SLEEP)
+        
+        queue_name, msg = db.brpop(settings.REDIS_QUEUE)
+        msg = json.loads(msg)
+        image_name = msg['image_name']
+
+        pred_class, pred_score = predict(image_name)
+
+        output_msg = {
+            'prediction': pred_class, 
+            'score': pred_score
+            }
+
+        db.set(msg['id'], json.dumps(output_msg))
 
 
 if __name__ == "__main__":
