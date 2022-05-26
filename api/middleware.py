98bc14c68da15b5f73053a84590be2e7cae3c9bc
Jose I Gonzalez B <jose6020_557@hotmail.com>
2022-05-26 16:36:41 -0300

    Implemented classify_process

--- api/middleware.py
+++ api/middleware.py
@@ -2,15 +2,14 @@ import json
 import redis
 import settings
 import uuid
+import time
 
-# TODO
-# Connect to Redis and assign to variable `db``
-# Make use of settings.py module to get Redis settings like host, port, etc.
+
 db = redis.Redis(
     host = settings.REDIS_IP,
     port = settings.REDIS_PORT, 
     db = settings.REDIS_DB_ID
-    )
+)
 
 def model_predict(image_name):
     """
--- model/ml_service.py
+++ model/ml_service.py
@@ -1,12 +1,14 @@
+import json
 import time
-
+import redis
 import settings
 
 
-# TODO
-# Connect to Redis and assign to variable `db``
-# Make use of settings.py module to get Redis settings like host, port, etc.
-db = None
+db = redis.Redis(
+    host = settings.REDIS_IP,
+    port = settings.REDIS_PORT, 
+    db = settings.REDIS_DB_ID
+)
 
 # TODO
 # Load your ML model and assign to variable `model`
@@ -41,9 +43,6 @@ def classify_process():
     model to get predictions and stores the results back in Redis using
     the original job ID so other services can see it was processed and access
     the results.
-
-    Load image from the corresponding folder based on the image name
-    received, then, run our ML model to get predictions.
     """
     while True:
         # Inside this loop you should add the code to:
@@ -61,7 +60,21 @@ def classify_process():
         #       code with Redis making use of functions `brpop()` and `set()`.
         # TODO
 
-        # Don't forget to sleep for a bit at the end
+        _, job_data_str = db.brpop(settings.REDIS_QUEUE)
+        job_data = json.loads(job_data_str)
+
+        pred_class, pred_score = predict(job_data["image_name"])
+
+        pred_data = {
+            "prediction": pred_class,
+            "score": pred_score
+        }
+
+        pred_data_str = json.dumps(pred_data)
+
+        db.set(job_data["id"], pred_data_str)
+
+        
         time.sleep(settings.SERVER_SLEEP)
 
 
