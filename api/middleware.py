d934753b3c4255e5f3ab41f08beb79c63f2f6adf
Jose Luis <cabrera@inkuba.com>
2022-05-25 23:30:56 -0300

    api create middleware

--- api/middleware.py
+++ api/middleware.py
@@ -1,12 +1,19 @@
 import time
-
+import redis
 import settings
+import uuid
+import json
 
 # TODO
 # Connect to Redis and assign to variable `db``
 # Make use of settings.py module to get Redis settings like host, port, etc.
-db = None
+db = redis.Redis(
+    host= settings.REDIS_IP, 
+    port= settings.REDIS_PORT, 
+    db= settings.REDIS_DB_ID
+)
 
+assert db.ping, "Unable to connect to redis"
 
 def model_predict(image_name):
     """
@@ -27,8 +34,8 @@ def model_predict(image_name):
     # Assign an unique ID for this job and add it to the queue.
     # We need to assing this ID because we must be able to keep track
     # of this particular job across all the services
-    # TODO
-    job_id = None
+
+    job_id = str(uuid.uuid4())
 
     # Create a dict with the job data we will send through Redis having the
     # following shape:
@@ -36,25 +43,34 @@ def model_predict(image_name):
     #    "id": str,
     #    "image_name": str,
     # }
-    # TODO
-    job_data = None
+    
+    job_data = {
+        "id": job_id,
+        "image_name": image_name
+    }
 
     # Send the job to the model service using Redis
     # Hint: Using Redis `rpush()` function should be enough to accomplish this.
-    # TODO
+
+    db.rpush('app_queue', json.dumps(job_data))
 
     # Loop until we received the response from our ML model
     while True:
         # Attempt to get model predictions using job_id
         # Hint: Investigate how can we get a value using a key from Redis
-        # TODO
-        output = None
+        
+        #output = db.get(job_id)
 
-        # Don't forget to delete the job from Redis after we get the results!
-        # Then exit the loop
-        # TODO
+        if(db.exists(job_id)):
+            output = db.get(job_id)
+            prediction = json.loads(output)
+            score = prediction['score']
+            class_name = prediction['class_name']
+            db.delete(job_id)
+            break
+
 
         # Sleep some time waiting for model results
         time.sleep(settings.API_SLEEP)
 
-    return None, None
+    return class_name, score
