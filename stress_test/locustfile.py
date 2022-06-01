c01d0b58a3c0111da5dda561e516a91d96428864
gabriel <rappi@MacBook-Pro-de-rappi.local>
2022-06-01 14:27:35 -0300

    stress test

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,12 +1,31 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task , between
 
 
 class UserBehavior(TaskSet):
 
     # Put your stress tests here
     # TODO
-    raise NotImplementedError
+    #raise NotImplementedError
+    @task(1)
+    def index(self):
+        self.client.get("http://0.0.0.0/")
+
+    @task(3)
+    def predict(self):
+        files= [
+            ('file',('dog.jpeg',open('dog.jpeg','rb'),'image/jpeg'))
+        ]
+        headers={}
+        payload={}
+
+        self.client.post("http://0.0.0.0/predict",
+        headers=headers,
+        data=payload,
+        files=files
+        )
+
+class APIUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5) #for a random time between a min and max value in seconds
 
 
-class APIUser(HttpLocust):
-    task_set = UserBehavior
