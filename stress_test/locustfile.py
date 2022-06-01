ff79a98238e4ef9f52cdc3a1683dd58109280bee
fede6590 <federicoferreyra65@gmail.com>
2022-06-01 18:13:07 -0300

    locust working & first changes in html front end

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,12 +1,16 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task
+
+class UserBehavior(HttpUser):
+
+    @task(1)
+    def index(self):
+        self.client.get("http://localhost")
+
+    @task(3)
+    def predict(self):
+        files = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))]
+        self.client.post("http://localhost/predict", files=files)
 
 
-class UserBehavior(TaskSet):
-
-    # Put your stress tests here
-    # TODO
-    raise NotImplementedError
-
-
-class APIUser(HttpLocust):
+class APIUser(HttpUser):
     task_set = UserBehavior
