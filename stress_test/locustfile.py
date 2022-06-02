e2adb52d11897740b739b4ba26740de609731051
Ine Picone <inespicone@hotmail.com>
2022-06-02 10:03:27 -0300

    locust comntada

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -11,18 +11,13 @@ class UserBehavior(HttpUser):
  
     @task(3) # el 3 quiere decir que por cada vez que se ejecute el otro task 
     def predict(self):
+        # le paso una lista con una tupla --> es una lista de un elemento y ese elemento es una tupla.
+        # a este endpoint se le pasa un archivo (el que euqeremos predecir) por eso ademas del url le paso file.
         files = [('file',('dog.jpeg', open('dog.jpeg', 'rb'), 'image/jpeg'))] 
         headers = {}
         payload = {}
         self.client.post('http://localhost/predict', headers = headers, data = payload, files = files)
 
-    # dont we need an on_start_method?
-    
 
-#class APIUser(HttpUser):
-#    task_set = UserBehavior
-#    min_wait = 1000
-#    max_wait = 5000
-#
 
     
