4f46d0e3ed2429083513989225a7f34c588c4eda
Ricardo Soria <rhemsy@gmail.com>
2022-06-03 16:40:23 -0300

    Stess Test fix

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,17 +1,22 @@
-from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, task, between
 
-wait_time = between(1, 5)
+class UserBehavior(HttpUser):
+    wait_time = between(0.5, 1)
 
-class UserBehavior(TaskSet):
     @task(1)
     def index(self):
         self.client.get("http://localhost/")
 
     @task(3)
     def predict(self):
-        self.client.get("http://localhost/")
         header  = {}
         data    = {}
-        files   = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))]
-        self.client.post("http://localhost/predict", header = header, data = data, files = files)
-        
+        files   = [
+            ("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg")),
+        ]
+        self.client.post(
+            "http://localhost/predict",
+            header = header,
+            data = data,
+            files = files,
+        )
