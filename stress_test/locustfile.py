9a470a1fd6af7446d858e666d7563e2c7b0708a6
Ricardo Soria <rhemsy@gmail.com>
2022-06-03 16:17:05 -0300

    Stress test FIX

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,34 +1,17 @@
-# import time
-# from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, TaskSet, task, between
 
-# wait_time = between(1, 5)
+wait_time = between(1, 5)
 
-
-# class UserBehavior(TaskSet):
-#     @task(1)
-#     def index(self):
-#         self.client.get("http://localhost/")
-
-#     @task(3)
-#     def predict(self):
-#         self.client.get("http://localhost/")
-#         files   = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))]
-#         self.client.post("http://localhost/predict", header = {}, data = {}, files = files)
-# class APIUser(HttpUser):
-#     tasks = [UserBehavior]
-from locust import HttpUser, task
-
-
-class UserBehavior(HttpUser):
-    max_wait = 5000
-    min_wait = 1000
-
+class UserBehavior(TaskSet):
     @task(1)
     def index(self):
-        self.client.get('http://localhost/')
-    
+        self.client.get("http://localhost/")
 
-    @task(2)
+    @task(3)
     def predict(self):
-        file = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))]
-        self.client.post('http://localhost/predict', headers = {}, data = {}, files = file)
+        self.client.get("http://localhost/")
+        header  = {}
+        data    = {}
+        files   = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))]
+        self.client.post("http://localhost/predict", header = header, data = data, files = files)
+        
