4ff21b33fdeb9d6ac62cbb0b7bd001631513254e
JulietaPerez <perezfranco.julieta@gmail.com>
2022-06-03 22:28:45 +0200

    Stress test and Report of localhoste and locust

---
+++ stress_test/PEREZ - Report SP4.md
@@ -0,0 +1,35 @@
+# Stress Test Report
+
+##### PC = Intel(R)Core(TM)i5 Cores=4 RAM=8G
+
+## 1° Stress Test:
+        - Number of users: 50
+        - Rate: 20
+
+Here I'm testing 50 users, max wait time in this case is 47.000 m secs
+
+![Test 50 users](/sprint-4-project/stress_test/50-20.png)
+
+## 2° Stress Test:
+        - Number of users: 200
+        - Rate: 10
+
+Here I'm testing 200
+
+![Test 200 users](/sprint-4-project/stress_test/200-10.png)
+
+## Scaled Model:
+        - Number of users: 50
+        - Rate: 20
+
+Here I'm testing the scaled model with 50 
+
+![Scaled 50 users](/sprint-4-project/stress_test/Scaling50-20.png)
+
+## Scaled Model 2:
+        - Number of users: 8
+        - Rate: 2
+
+Here I'm testing the scaled model only 8 users were I can see that the model runs perfeclty without leaving users with no response and, accomplishing time response at 0 m secs
+
+![Scaled 8 users](/sprint-4-project/stress_test/Scaling8-1.png)
--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,12 +1,37 @@
-from locust import HttpLocust, TaskSet, task
-
+from locust import TaskSet, task, HttpUser, between
 
 class UserBehavior(TaskSet):
-
     # Put your stress tests here
     # TODO
-    raise NotImplementedError
+    # raise NotImplementedError
+
+    @task(1)
+    ## call the "page" like a reload
+    ## 1 excute this task only once
+    def index(self):
+        self.client.get('http://localhost/')
+
+    @task(3)
+    # what servidor has to do with clients
+    ## executed 3 times
+    ## automatization
+    def predict(self):
+        ## recive a file type image jpg, and dog image, open it. Send it to the local host
+        file = [
+            ('file', ('dog.jpeg', open('dog.jpeg','rb'), 'image/jpeg'))
+        ]
+        headers = {}
+        payload = {}
+        self.client.post('http://localhost/predict',
+            headers = headers,
+            data = payload,
+            files = file,
+        )
 
 
-class APIUser(HttpLocust):
-    task_set = UserBehavior
+class APIUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1,10)
+# older version
+#    min_wait = 1000
+#    max_wait = 5000
