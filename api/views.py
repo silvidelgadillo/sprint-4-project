0500f68cecdd1f1ec2f6ce8acb57515997757a80
fpellerano <fpellerano@gmail.com>
2022-06-06 18:25:05 -0300

    predict done

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 import utils
+from flask import current_app, flash, jsonify, make_response, redirect, request, url_for
 import os
 from middleware import model_predict
 import settings
@@ -46,17 +47,20 @@ def upload_image():
 
     # File received and it's an image, we must show it and get predictions
     if file and utils.allowed_file(file.filename):
-        #   1. Get an unique file name using utils.get_file_hash() function        
+        #   1. Get an unique file name using utils.get_file_hash() function
+        # https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/        
         newfile = utils.get_file_hash(file)
 
         #   2. Store image (os.path so as not to remplace images if they are the same)
-        if os.path.exists(newfile) == False: 
+        if not os.path.exists(os.path.join(settings.UPLOAD_FOLDER, newfile)):
             file.save(os.path.join(settings.UPLOAD_FOLDER, newfile)) 
         #   3. Send the file to be processed by the `model` service
         #      Hint: Use middleware.model_predict() for sending jobs to model
         #            service using Redis.
         model_pred, model_score = model_predict(newfile)        
         #   4. Update `context` dict with the corresponding values
+        model_pred = predict.replace("_"," ").title()
+        model_score = round(model_score*100, 2)
         context = {
             "prediction": model_pred,
             "score": model_score,
@@ -113,41 +117,53 @@ def predict():
     """
     # To correctly implement this endpoint you should:
     #   1. Check a file was sent and that file is an image
-    #   2. Store the image to disk
-    #   3. Send the file to be processed by the `model` service
-    #      Hint: Use middleware.model_predict() for sending jobs to model
-    #            service using Redis.
-    #   4. Update and return `rpse` dict with the corresponding values
-    # If user sends an invalid request (e.g. no file provided) this endpoint
-    # should return `rpse` dict with default values HTTP 400 Bad Request code
-    # TODO
-
+    # mensaje de salida 
     rpse = {"success": False, "prediction": None, "score": None}    
     
+    # si no recibe archivo
     if "file" not in request.files:
-        flash("No file part")
-        return redirect(request.url)
+        return jsonify(rpse), 400 #400  status code
 
+    # Recibe una imagen pero no es tipo "file"
     file = request.files["file"]
     if file.filename == "":
-        flash("No image selected for uploading")
-        return redirect(request.url)
+        return jsonify(rpse), 400 #400  status code
 
-    # File received and but it isn't an image
-    else:
-        flash("Allowed image types are -> png, jpg, jpeg, gif")
-        return redirect(request.url)
-    
+    #   2. Store the image to disk
+    # File received and it's an image, we must show it and get predictions
+    if file and utils.allowed_file(file.filename):
+        #   1. Get an unique file name using utils.get_file_hash() function
+        # https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/        
+        newfile = utils.get_file_hash(file)
 
+        #   2. Store image (os.path so as not to remplace images if they are the same)
+        if os.path.exists(newfile) == False: 
+            if not os.path.exists(os.path.join(settings.UPLOAD_FOLDER, newfile)):
+                file.save(os.path.join(settings.UPLOAD_FOLDER, newfile)) 
+        #   3. Send the file to be processed by the `model` service
+        #      Hint: Use middleware.model_predict() for sending jobs to model
+        #            service using Redis.
+            model_pred, model_score = model_predict(newfile) 
+            rpse = {"success": True, "prediction": model_pred, "score": model_score}       
+        #   4. Update `context` dict with the corresponding values
+        context_2 = {
+            "success": True,
+            "prediction": model_pred,
+            "score": model_score
+        }        
+        
+        # Update `render_template()` parameters as needed
+        # TODO
 
-
-
+        return jsonify(context_2), 200
+    #if it is none of the above optionss
+    rpse = {"success": False, "prediction": None, "score": None}
+    return jsonify(rpse), 400
 
 @router.route("/feedback", methods=["GET", "POST"])
 def feedback():
     """
     Store feedback from users about wrong predictions on a text file.
-
     Parameters
     ----------
     report : request.form
@@ -157,18 +173,24 @@ def feedback():
                 "prediction": str,
                 "score": float
             }
-
         - "filename" corresponds to the image used stored in the uploads
           folder.
         - "prediction" is the model predicted class as string reported as
           incorrect.
         - "score" model confidence score for the predicted class as float.
     """
-    # Get reported predictions from `report` key
+    # Get reported predictions from `report` key
     report = request.form.get("report")
-
     # Store the reported data to a file on the corresponding path
-    # already provided in settings.py module
-    # TODO
+    # already provided in settings.py module
+
+    with open(settings.FEEDBACK_FILEPATH, "a+") as feedback:
+        # parametro 'a+' - (a) si no existe el archivo, lo crea. si existe lo abre y lo lee
+        feedback.write(str(report) + "\n")
+
+    # Alan
+    # feedback = open(settings.FEEDBACK_FILEPATH, "a+")
+    # feedback.write(str(report) + '\n')
+    # feedback.close()
 
     return render_template("index.html")
--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,12 +1,30 @@
-from locust import HttpLocust, TaskSet, task
+from email.mime import image
+import time
+from wsgiref import headers
+from locust import TaskSet, task, HttpUser, between
 
 
 class UserBehavior(TaskSet):
+    # se define la estategia de interacción de los usuarios
+    @task(1) # el 1 es que dará prio al index contra el predict de 1 a 3 veces
+    def index(self):
+        self.client.get("http://localhost/") #simple get
 
+    @task(3)
+    def predict(self): # le enviamos un archivo por metodo post 
     # Put your stress tests here
-    # TODO
-    raise NotImplementedError
-
-
-class APIUser(HttpLocust):
-    task_set = UserBehavior
+        files = [
+            ("file",("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))
+        ]
+        header = {}
+        payload = {}
+        self.client.post(
+            "http://localhost/predict/",
+            headers=headers,
+            data=payload,
+            files=files,
+        )
+# antes que el usuario se conecta al endpoint tiene que esperar min wait, y el usuario  tiene que esperar max wait para un nuevo  request 
+class APIUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1,10)
