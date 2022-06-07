056f43f73dad0650b8477474750b9305bef87f80
Marcos Ziegler <ziegler.marcos.b@gmail.com>
2022-06-07 00:53:26 -0300

    feat: Add locustfile
    Add locustfile with "/" and "/predict" endpoint testing.

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,12 +1,26 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task, between
 
 
 class UserBehavior(TaskSet):
 
     # Put your stress tests here
-    # TODO
-    raise NotImplementedError
-
-
-class APIUser(HttpLocust):
-    task_set = UserBehavior
+    @task
+    def predict_post(self):
+        with open("dog.jpeg", "rb") as file:
+            url = 'http://localhost/predict'
+            filename = 'dog.jpeg'
+            headers = {
+                    # 'User-Agent': 'curl/7.58.0',
+                    # 'Cookie': 'authsomething=datastring; othercookie=datastring',
+            }
+            files = [
+                ("image", (filename, file, "image/jpeg")),
+            ]
+            response = self.client.post(url, headers=headers, files=files)
+    @task
+    def index_get(self):
+        url = 'http://localhost'
+        response = self.client.get(url)
+class MyLocust(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
